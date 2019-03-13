1ce9ce0aef7feaa0500055e4acc401e72b3b58d8
Jungho Ro <junghoro@Junghos-MacBook-Pro.local>
2019-03-13 09:21:41 -0500

    python-challenge

---
+++ PyBank/main.py
@@ -0,0 +1,70 @@
+import os
+import csv
+
+# Creating a path to pull data from folder (Googled for ".expanduser" function to read the file)
+budgetCSV = os.path.expanduser('~/Desktop/Homework 3/python-challenge/budget_data.csv')
+
+# Reading the CSV file
+with open(budgetCSV, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    
+    # Define the Variables and Starting values
+    firstvalue = next(csvreader)
+    row = 0
+    totalMonth = 1
+    totalRevenue = int(firstvalue[1])
+    revenueChange = 0
+    currentRev = int(firstvalue[1])
+    previousRev = 0
+    lowestRev = int(firstvalue[1])
+    greatestRev = int(firstvalue[1])
+    totalRevchange = 0
+
+    # Loop through the data
+    for row in csvreader:
+        
+        # Total Months
+        totalMonth += 1
+
+        # Total Profits/Losses 
+        totalRevenue += int(row[1])
+
+        # Tracking Month to Month Profit/Losses Change
+        previousRev = currentRev
+        currentRev = int(row[1])
+        revenueChange = currentRev - previousRev
+        totalRevchange += revenueChange
+
+        # Finding Greatest Decrease & Increase in Profit/Losses
+        if revenueChange > greatestRev:
+            greatestRev = revenueChange
+            greMonth = str(row[0])
+        if revenueChange < lowestRev:
+            lowestRev = revenueChange
+            lowMonth = str(row[0])
+    
+    # Calculate Average Change of Profit/Losses
+    averagechange = totalRevchange/(totalMonth-1)
+
+# Print Final Results in Terminal
+print("Financial Analysis")
+print("----------------------------------")
+print(f"Total Months: {str(totalMonth)}")
+print(f"Total: ${str(totalRevenue)}")
+print(f"Average Change: ${str(round(averagechange, 2))}")
+print(f"Greatest Increase in Profits: {greMonth} $({str(greatestRev)})")
+print(f"Greatest Decrease in Proftis: {lowMonth} $({str(lowestRev)})")
+
+# Specify where to export Text File to
+export_file = os.path.expanduser('~/Desktop/Homework 3/python-challenge/PyBank/Final.txt')
+
+# Open the file and write what to print in Text File (Googled "\n" so it prints in the next line)
+with open(export_file, 'w') as file:
+    file.write("Financial Analysis\n")
+    file.write("----------------------------------\n")
+    file.write(f"Total Months: {str(totalMonth)}\n")
+    file.write(f"Total: ${str(totalRevenue)}\n")
+    file.write(f"Average Change: ${str(round(averagechange, 2))}\n")
+    file.write(f"Greatest Increase in Profits: {greMonth} $({str(greatestRev)})\n")
+    file.write(f"Greatest Decrease in Proftis: {lowMonth} $({str(lowestRev)})\n")
---
+++ PyPoll/main.py
@@ -0,0 +1,76 @@
+import os
+import csv
+
+# Creating a path to pull data from folder (Once again used ".expanduser" to read the file)
+electionCSV = os.path.expanduser('~/Desktop/Homework 3/python-challenge/election_data.csv')
+
+# Reading the CSV file
+with open(electionCSV, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    # Define the Variables and Starting values
+    row = 0
+    totalVotes = 0
+    khanVote = 0
+    correyVote = 0
+    liVote = 0
+    otooleyVote = 0
+
+    # Loop through the data
+    for row in csvreader:
+
+        # Total amount of votes
+        totalVotes += 1
+
+        # Finding the total votes for each Candidate
+        if row[2] == "Khan":
+            khanVote += 1
+        elif row[2] == "Correy":
+            correyVote += 1
+        elif row[2] == "Li":
+            liVote += 1
+        elif row[2] == "O'Tooley":
+            otooleyVote += 1
+   
+    # Calculate Percentage of Votes for each Candidate
+    khanPercent = (khanVote / totalVotes) * 100
+    correyPercent = (correyVote / totalVotes) * 100
+    liPercent = (liVote / totalVotes) * 100
+    otooleyPercent = (otooleyVote / totalVotes) * 100
+
+    # Creating Dictionary and finding Winner of election (Googled for max function and key=__.get to find value)
+    candidates = ["Khan", "Correy", "Li", "O'Tooley"]
+    candidatevotes = [khanVote, correyVote, liVote, otooleyVote]
+    candidatelist = dict(zip(candidates, candidatevotes))
+    winner = max(candidatelist, key=candidatelist.get)
+
+# Print Final Results in Terminal
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {str(totalVotes)}")
+print("-------------------------")
+print(f"Khan: {str(round(khanPercent, 3))}% ({str(khanVote)})")
+print(f"Correy: {str(round(correyPercent, 3))}% ({str(correyVote)})")
+print(f"Li: {str(round(liPercent, 3))}% ({str(liVote)})")
+print(f"O'Tooley: {str(round(otooleyPercent, 3))}% ({str(otooleyVote)})")
+print("-------------------------")
+print(f"Winner: {str(winner)}")
+print("-------------------------")
+
+# Specify where to export Text File to
+export_file = os.path.expanduser('~/Desktop/Homework 3/python-challenge/PyPoll/Final.txt')
+
+# Open the file and write what to print in Text File (Once agained use "\n" so it prints in the next line)
+with open(export_file, 'w') as file:
+    file.write("Election Results\n")
+    file.write("-------------------------\n")
+    file.write(f"Total Votes: {str(totalVotes)}\n")
+    file.write("-------------------------\n")
+    file.write(f"Khan: {str(round(khanPercent, 3))}% ({str(khanVote)})\n")
+    file.write(f"Correy: {str(round(correyPercent, 3))}% ({str(correyVote)})\n")
+    file.write(f"Li: {str(round(liPercent, 3))}% ({str(liVote)})\n")
+    file.write(f"O'Tooley: {str(round(otooleyPercent, 3))}% ({str(otooleyVote)})\n")
+    file.write("-------------------------\n")
+    file.write(f"Winner: {str(winner)}\n")
+    file.write("-------------------------\n")
